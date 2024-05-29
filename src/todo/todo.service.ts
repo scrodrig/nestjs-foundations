33285cb318381628b9e2d15547c6a3d09068f70f
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-05-29 16:20:57 +0100

    chore: Update UpdateTodoDto with optional 'done' property

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -34,7 +34,19 @@ export class TodoService {
   }
 
   update(id: number, updateTodoDto: UpdateTodoDto) {
-    return `This action updates a #${id} todo`;
+    const { done, description } = updateTodoDto;
+
+    const todo = this.findOne(id);
+
+    if (done !== undefined) todo.done = done;
+    if (description) todo.description = description;
+
+    this.todos = this.todos.map((dbTodo) => {
+      if (dbTodo.id === id) return todo;
+      return dbTodo;
+    });
+
+    return todo;
   }
 
   remove(id: number) {
